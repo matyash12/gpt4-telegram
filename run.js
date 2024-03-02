c54dd9b2284fb04f8658d514ce942fc82f8a079d
matyash12 <hajekmatyas1@gmail.com>
2024-03-02 17:33:43 +0100

    Making only for one user

--- run.js
+++ run.js
@@ -9,6 +9,8 @@ const openai = new OpenAI({ apiKey: openaiKey });
 const token = process.env.TELEGRAMBOTKEY;
 const bot = new TelegramBot(token, { polling: true });
 
+const userId = process.env.AUTHORIZEDUSER
+
 const openaiModel = process.env.OPENAIMODELTOUSER
 
 bot.onText(/\/echo (.+)/, (msg, match) => {
@@ -18,15 +20,25 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 });
 
 async function answerMessage(msg) {
-    console.log("The message the user sent: " + msg.text)
+    //console.log("The message the user sent: " + msg.text)
     const completion = await openai.chat.completions.create({
         messages: [{ role: "user", content: msg.text }],
         model: openaiModel,
     });
-    console.log("Message to be sent to user: " + completion.choices[0].message.content)
+    //console.log("Message to be sent to user: " + completion.choices[0].message.content)
+    const currentTime = new Date().toLocaleString();
+    console.log(`[${currentTime}] Some message asnwered.`);
     bot.sendMessage(msg.chat.id, completion.choices[0].message.content)
 }
 
 bot.on('message', (msg) => {
-    answerMessage(msg);
+    const currentTime = new Date().toLocaleString();
+    console.log(`[${currentTime}] Some message received.` + 'msg.chat.id: '+msg.chat.id);
+
+    if (msg.from.id == userId){
+        answerMessage(msg);
+    }else{
+        bot.sendMessage(msg.chat.id,"Sorry i can't answer to you! Btw your msg.from.id is '"+msg.from.id+"'");
+    }
+
 });
